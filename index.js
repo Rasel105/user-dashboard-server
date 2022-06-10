b558fb0382127f34d924eb557ffd380b42ddd36e
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-06-10 17:19:09 +0600

    get and post api created

--- index.js
+++ index.js
@@ -13,9 +13,37 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.sz491.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const IOTCollection = client.db("user-dashboard").collection("products");
+
+        // GET ALL Product 
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        // POST api 
+        app.post("/product", async (req, res) => {
+            const newProduct = req.body;
+            const result = await IOTCollection.insertOne(newProduct);
+            res.send(result);
+        });
+
+
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
