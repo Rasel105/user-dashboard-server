0d6a501a0efa0748b1c3c134a258d29fe74409bb
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-06-11 12:18:44 +0600

    product deleted successfully

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
