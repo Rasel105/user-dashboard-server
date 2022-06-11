3d2d0f1c4ad64707b0414701d9249bc105216219
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-06-11 12:06:45 +0600

    delete api added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // user: Rasel
-// S9OTUipT4hgH6IRh
+// hjdFKU7yJKkXS0d6
 const express = require('express');
 const cors = require('cors');
 // environment variable setup
@@ -12,9 +12,9 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-
-const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.sz491.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.2aeqs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 async function run() {
     try {
@@ -22,29 +22,36 @@ async function run() {
         const IOTCollection = client.db("user-dashboard").collection("products");
 
         // GET ALL Product 
-
-        app.get('/products', async (req, res) => {
+        app.get('/items', async (req, res) => {
             const query = {};
-            const cursor = productCollection.find(query);
+            const cursor = IOTCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         });
 
         // POST api 
-        app.post("/product", async (req, res) => {
-            const newProduct = req.body;
-            const result = await IOTCollection.insertOne(newProduct);
+        app.post('/item', async (req, res) => {
+            const newItem = req.body;
+            console.log(newItem)
+            const result = await IOTCollection.insertOne(newItem);
+            res.send(result);
+        });
+
+        // DELETE api 
+
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await IOTCollection.deleteOne(query);
             res.send(result);
         });
 
 
     } finally {
-        await client.close();
+
     }
 }
 run().catch(console.dir);
-
-
 
 app.get('/', (req, res) => {
     res.send("Running Genius Server");
